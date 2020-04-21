92f82de3d6b0d793d9efe78b07d6ce0b6d75fae9
Mia112 <msheriff112@gmail.com>
2020-04-21 16:39:07 -0400

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,24 @@ import {
 	Redirect,
 	Switch,
 } from 'react-router-dom';
+import MainNav from './shared/components/Navigation/MainNav';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 const App = () => {
 	return (
 		<Router>
-			<Switch>
-				<Route path='/' exact>
-					<Users />
-				</Route>
-				<Route path='/places/new' exact>
-					<NewPlace />
-				</Route>
-				<Redirect to='/' />
-			</Switch>
+			<MainNav />
+			<main>
+				<Switch>
+					<Route path='/' exact>
+						<Users />
+					</Route>
+					<Route path='/places/new' exact>
+						<NewPlace />
+					</Route>
+					<Redirect to='/' />
+				</Switch>
+			</main>
 		</Router>
 	);
 };
--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './UsersList.css';
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIElememts/Card';
+import './UsersList.css';
 const UserList = (props) => {
 	if (props.items.length === 0) {
 		return (
 			<div className='center'>
-				<h2>No Users Found</h2>
+				<Card>
+					<h2>No Users Found</h2>
+				</Card>
 			</div>
 		);
 	}
